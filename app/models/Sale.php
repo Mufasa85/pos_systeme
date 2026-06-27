a66142f48e7a71f5bb572f6dde56743c670dbd60
hydra <16paysayocesar@gmail.com>
2026-06-27 07:00:35 +0100

    feat : add multi pay column

--- app/models/Sale.php
+++ app/models/Sale.php
@@ -15,14 +15,15 @@ class Sale
 
     public function create($data)
     {
-        $sql = "INSERT INTO ventes (numero_facture, client_id, sous_total_ht, tva, total, vendeur_id, date, dateDGI, qrCode, codeDEFDGI, counters, nim, comment, service) 
-                VALUES (:numero_facture, :client_id, :sous_total_ht, :tva, :total, :vendeur_id, :date, :dateDGI, :qrCode, :codeDEFDGI, :counters, :nim, :comment, :service)";
+        $sql = "INSERT INTO ventes (numero_facture, client_id, sous_total_ht, tva, total, payments, vendeur_id, date, dateDGI, qrCode, codeDEFDGI, counters, nim, comment, service) 
+                VALUES (:numero_facture, :client_id, :sous_total_ht, :tva, :total, :payments, :vendeur_id, :date, :dateDGI, :qrCode, :codeDEFDGI, :counters, :nim, :comment, :service)";
         $this->db->query($sql, [
             ':numero_facture' => $data['numero_facture'],
             ':client_id'      => $data['client_id'] ?? null,
             ':sous_total_ht'  => $data['sous_total_ht'],
             ':tva'            => $data['tva'],
             ':total'          => $data['total'],
+            ':payments'       => isset($data['payments']) ? json_encode($data['payments']) : null,
             ':vendeur_id'     => $data['vendeur_id'],
             ':date'           => $data['date'],
             ':dateDGI'        => $data['dateDGI'] ?? null,
@@ -39,7 +40,7 @@ class Sale
     public function getAllSales()
     {
         $sql = "SELECT v.*, u.nom_complet as nom_vendeur, 
-                       c.nom_client, c.code_client, c.numero as client_numero, c.nif as client_nif,
+                       c.nom_client, c.code_client, c.numero as client_numero, c.nif as client_nif, c.adresse as client_adresse,
                        tc.code as client_type_code
                  FROM ventes v 
                  LEFT JOIN utilisateurs u ON v.vendeur_id = u.id 
@@ -72,7 +73,7 @@ class Sale
     public function exist($id)
     {
         $sql = "SELECT v.*, u.nom_complet as nom_vendeur, 
-                       c.nom_client, c.code_client, c.numero as client_numero, c.nif as client_nif,
+                       c.nom_client, c.code_client, c.numero as client_numero, c.nif as client_nif, c.adresse as client_adresse,
                        tc.code as client_type_code
                 FROM ventes v 
                 LEFT JOIN utilisateurs u ON v.vendeur_id = u.id 
